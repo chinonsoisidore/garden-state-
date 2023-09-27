5f68e72fe3d361ccd1022e108cfe8629bb748efb
chinonsoisidore <chinonsoisidore525@gmail.com>
2023-09-27 03:14:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// let addEl = document.querySelector("#add");
+// let headingEl = document.querySelector(".heading");
+// addEl.addEventListener("mouseover", () =>{
+//     console.log("hello")
+//     headingEl.style.color = "red"
+// })
